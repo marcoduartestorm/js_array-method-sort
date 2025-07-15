ea33b2f1fabed815132fdf2d51bfefaa6d3e74d9
marcoduartestorm <marcosbottocomp@gmail.com>
2025-07-15 08:29:26 -0300

    Soluç~çao

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,15 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < vet.length; i++) {
+      for (let j = i; j < vet.length; j++) {
+        if (compareFunction(vet[i], vet[j]) > 0) {
+          let temp = vet[i];
+          vet[i] = vet[j];
+          vet[j] = temp;
+        }
+      }
+    }
   };
 }
 
